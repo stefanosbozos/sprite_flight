39e0123f791e8eea63bcdc442cd630566b95b169
stefanosbozos <stefbozos@hotmail.com>
2025-07-01 14:44:55 +0100

    refined the reload and changed it to cooldown - Finished with the player shooting system - next stop enemies

--- Assets/Scripts/ShootingSystem.cs
+++ Assets/Scripts/ShootingSystem.cs
@@ -9,79 +9,96 @@ using UnityEngine.InputSystem;
 public class ShootingSystem : MonoBehaviour
 {
 
-    [Header("Projectile")]
+    // Laser System Variables
+    const float HEAT_LIMIT = 100.0f;
+    private bool laserOverheated = false;
+    // The temperature of the laser
+    private float laserTemp = 0;
+
+    [Header("Laser System")]
     [SerializeField] private Rigidbody2D laserProjectile;
-    [SerializeField] private int ammoAmount = 10;
-    [SerializeField] private float reloadTime = 0f;
-    [SerializeField] private float reloadCooldown = 0.5f;
-    private bool magazineIsEmpty = false;
-    private bool initiateReloadAction = false;
+    // The amount of heat that the laser can withstand before shutting down to cool off
+    [SerializeField] private float cooldownTime = 3f;
+    // The step that the laser coolsdown while not overheated and not fired
+    [SerializeField] private float coolingOffStep = 3f;
+
+
 
     // Player Input system
     Camera mainCam;
     Vector3 mousePos;
     InputAction shoot;
-    InputAction reload;
+    // This is used to make sure that the mouse is aiming from the Y axis of the player.
     private float rotationOffset = 90.0f;
 
     void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-
         shoot = InputSystem.actions.FindAction("Shoot");
-        reload = InputSystem.actions.FindAction("Reload");
     }
 
     void Update()
     {
         Aim();
         Shoot();
-        Reload();
+        LaserHeatingCheck();
     }
 
-    void Aim()
-    {
-        mousePos = mainCam.ScreenToWorldPoint(Mouse.current.position.value);
 
+    // The aiming system to point the Y axis of the player where the mouse points to.
+    void Aim()
+    {        
+        mousePos = mainCam.ScreenToWorldPoint(Mouse.current.position.value);
         Vector3 playerRotation = mousePos - transform.position;
 
         float rotationZ = Mathf.Atan2(playerRotation.y, playerRotation.x) * Mathf.Rad2Deg;
-
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ - rotationOffset);
     }
 
+    // Instantiate a laser PreFab on the screen
     void Shoot()
     {
-        if (shoot.WasPressedThisFrame() && !magazineIsEmpty)
+        if (shoot.WasPressedThisFrame())
         {
-            // Solution at https://discussions.unity.com/t/spawning-a-projectile-in-front-of-a-player-based-on-player-rotation/165403
-            Instantiate(laserProjectile, transform.Find("LaserSpawn").position, transform.Find("LaserSpawn").rotation);
-            ammoAmount -= 1;
-            Debug.Log("Ammo:" + ammoAmount);
+            if (!laserOverheated)
+            {
+                // Solution at https://discussions.unity.com/t/spawning-a-projectile-in-front-of-a-player-based-on-player-rotation/165403
+                Instantiate(laserProjectile, transform.Find("LaserSpawn").position, transform.Find("LaserSpawn").rotation);
+                laserTemp += 10;
+            }
+            else
+            {
+                Debug.Log("Laser is overheated!!!");
+            }
         }
-        if (ammoAmount == 0)
+
+    }
+    
+    // Checks the heat levels of the laser.
+    void LaserHeatingCheck()
+    {
+
+        if (laserTemp >= 0 && !laserOverheated)
         {
-            magazineIsEmpty = true;
-            //Debug.Log("Out of Ammo - Reload");
+            laserTemp -= Time.deltaTime * coolingOffStep;
+        }
+
+        if (laserTemp >= HEAT_LIMIT)
+        {
+            laserOverheated = true;
+            WaitToCooldown();
         }
     }
 
-    void Reload()
+    // The cooling down of the laser if it is overheated.
+    void WaitToCooldown()
     {
-        // This works but is ugly and needs refactoring!!!
-        reloadTime += Time.deltaTime;
-        if (reload.WasPressedThisFrame())
+        cooldownTime -= Time.deltaTime;
+        if (laserOverheated && cooldownTime <= 0.0f)
         {
-            reloadTime = 0f;
-            initiateReloadAction = true;
-            Debug.Log("RELOADING!");
-        }
-        if (reloadTime >= reloadCooldown && magazineIsEmpty && initiateReloadAction)
-        {
-            ammoAmount = 3;
-            magazineIsEmpty = false;
-            initiateReloadAction = false;
-            Debug.Log("Ready!");
+            laserTemp = 0f;
+            laserOverheated = false;
+            cooldownTime = 3f;
         }
     }
 
