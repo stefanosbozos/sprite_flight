329fe9dec414637a8c7f4917077a6dd58cb1675a
stefanosbozos <stefbozos@hotmail.com>
2025-07-05 10:52:40 +0100

    working on enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private GameObject player;
+    
+    private Rigidbody2D rb;
+
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float timeBetweenShots = 1f;
+    private float deltaDistance;
+    private float timer = 0f;
+
+    [SerializeField] private float movement_speed = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        deltaDistance = Mathf.FloorToInt(Random.Range(5f, 20f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+        Shoot();
+    }
+
+    void FollowPlayer()
+    {
+
+        Debug.Log(deltaDistance);
+
+        // Go to the players position (Lurker's behaviour)
+        Vector3 distanceFromThePlayer = new Vector3(transform.position.x, transform.position.y - deltaDistance, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position - distanceFromThePlayer, movement_speed * Time.deltaTime);
+
+        // Change the rotation accoriding to the player's rotation to always face the player
+        Vector3 enemyRotation = player.transform.position;
+        float rotationZ = Mathf.Atan2(enemyRotation.y, enemyRotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, rotationZ + 90.0f), 100 * Time.deltaTime);
+    }
+
+    void Shoot()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeBetweenShots)
+        {
+            GameObject spawnedBullet = Instantiate(projectile, transform.Find("ProjectileSpawn").position, transform.Find("ProjectileSpawn").rotation);
+            timer = 0;
+            Destroy(spawnedBullet, 3f);
+        }
+    }
+}
---
+++ Assets/Scripts/MyProjectile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MyProjectile : MonoBehaviour
+{
+    [SerializeField] private float projectileSpeed = 1f;
+    [SerializeField] private GameObject projectilePreFab;
+    [SerializeField] Vector3 direction = Vector3.up;
+    private float timeToLive = 1.5f;
+
+    void Update()
+    {
+        projectilePreFab.transform.Translate(direction * projectileSpeed * Time.deltaTime);  
+        //rb.AddForce(transform.up * projectileSpeed);
+        Destroy(projectilePreFab, timeToLive);
+    }
+
+}
