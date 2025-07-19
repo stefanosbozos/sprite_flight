45803f75565db824b20587400e61384fce503ab5
stefanosbozos <stefbozos@hotmail.com>
2025-07-19 16:17:43 +0100

    working on the UI

---
+++ Assets/Scripts/GameHUD.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameHUD : MonoBehaviour
+{
+    [SerializeField] private UIDocument m_UIDoc;
+    [SerializeField] private PlayerStatsSO m_playerStats;
+
+    private VisualElement m_healthBar;
+    private Label m_healthPercentage;
+    private VisualElement m_shieldBar;
+    private Label m_shieldPercentage;
+    private VisualElement m_laserBar;
+    private Label m_laserPercentage;
+    private Label m_resources;
+
+    void Awake()
+    {
+        m_healthBar = m_UIDoc.rootVisualElement.Q<VisualElement>("HealthBarFill");
+        m_shieldBar = m_UIDoc.rootVisualElement.Q<VisualElement>("ShieldBarFill");
+        m_laserBar = m_UIDoc.rootVisualElement.Q<VisualElement>("LaserBarFill");
+
+        m_healthPercentage = m_UIDoc.rootVisualElement.Q<Label>("HealthBarPercentage");
+        m_shieldPercentage = m_UIDoc.rootVisualElement.Q<Label>("ShieldBarPercentage");
+        m_laserPercentage = m_UIDoc.rootVisualElement.Q<Label>("LaserBarPercentage");
+        m_resources = m_UIDoc.rootVisualElement.Q<Label>("Resources");
+    }
+
+    void Update()
+    {
+        UpdateHealthBar();
+        UpdateShieldBar();
+        UpdateLaserBar();
+        UpdateResources();
+    }
+
+    void UpdateHealthBar()
+    {
+        m_healthBar.style.width = Length.Percent(m_playerStats.Health);
+        m_healthPercentage.text = m_playerStats.Health + "/" + m_playerStats.MaxHealth;
+    }
+
+    void UpdateShieldBar()
+    {
+        m_shieldBar.style.width = Length.Percent(m_playerStats.Shield);
+        m_shieldPercentage.text = m_playerStats.Shield + "/" + m_playerStats.MaxShield;
+    }
+
+    void UpdateLaserBar()
+    {
+        // m_laserBar.style.width = Length.Percent();
+        // m_shieldPercentage.text = "";
+    }
+
+    void UpdateResources()
+    {
+        m_resources.text = "Resources: ";
+    }
+}
--- Assets/Scripts/Player/PlayerAiming.cs
+++ Assets/Scripts/Player/PlayerAiming.cs
@@ -5,6 +5,7 @@ public class PlayerAiming : MonoBehaviour
 {
     [SerializeField] private PlayerStatsSO m_playerStats;
 
+    private Rigidbody2D m_rb;
     private InputAction m_aim;
     private Vector3 aimValue;
     private int rotationOffset = 90;
@@ -12,6 +13,7 @@ public class PlayerAiming : MonoBehaviour
     void Awake()
     {
         m_aim = InputSystem.actions.FindAction("Aim");
+        m_rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -21,9 +23,9 @@ public class PlayerAiming : MonoBehaviour
 
     private void Aim()
     {
-        aimValue = m_aim.ReadValue<Vector2>();
+        aimValue = m_aim.ReadValue<Vector2>().normalized;
 
-        if (aimValue.sqrMagnitude > 0.01f)
+        if (aimValue.sqrMagnitude > 0.1f)
         {
             float angle = Mathf.Atan2(aimValue.y, aimValue.x) * Mathf.Rad2Deg;
             Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle - rotationOffset);
